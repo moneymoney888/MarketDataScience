3de39936ed72191aa1fd8a6fa5fdb3992d2f246a
moneymoney888 <97957742+moneymoney888@users.noreply.github.com>
2022-01-19 11:56:24 +0800

    Update app.py

--- 行銷實用工具/telegramBot/app.py
+++ 行銷實用工具/telegramBot/app.py
@@ -7,8 +7,8 @@ from telegram.ext import Dispatcher, MessageHandler, Filters
 app = Flask(__name__)
 
 # 設定你的token
-bot = telegram.Bot(token=('你的token'))
-bot.send_message(chat_id = '你的ID', text ='你可以開始了')
+bot = telegram.Bot(token=('5086013813:AAGk0wFHc-SzlsRldUxFGr7xwS0oL9ahCF0'))
+bot.send_message(chat_id = '500642973', text ='你可以開始了')
 
 @app.route('/hook', methods=['POST'])
 def webhook_handler():
